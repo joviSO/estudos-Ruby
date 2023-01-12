38412f9f410c4721509299008d5138055015f1bb
João Victor Sousa Oliveira <joao.oliveira@maida.health>
2023-01-12 20:23:20 -0300

    adicionando mais um teste, adicionando configuraçao de documentaçao ao .rspec

--- tdd-ruby/spec/calculator/calculator_spec.rb
+++ tdd-ruby/spec/calculator/calculator_spec.rb
@@ -1,5 +1,23 @@
 require 'calculator'
 
 describe Calculator do
-  
+  context "#sum" do
+    it 'numeros positivos' do
+      calc = Calculator.new
+      result = calc.sum(5, 7)
+      expect(result).to eq(12)  
+    end
+
+    it 'com numeros positivos e negativos' do
+      calc = Calculator.new
+      result = calc.sum(5, -7)
+      expect(result).to eq(-2)  
+    end
+
+    it 'numeros negativos' do
+      calc = Calculator.new
+      result = calc.sum(-5, -7)
+      expect(result).to eq(-12)  
+    end
+  end
 end
